60346f40bcedf840a99a3187f32a38c90296c7cb
Alexis La Porte <aklaporte@gmail.com>
2016-10-11 19:19:52 -0400

    refactor conversion to base64

--- challenge1/solution1.py
+++ challenge1/solution1.py
@@ -1,10 +1,12 @@
 import base64
+import binascii
 
 def toBase64(hex_string):
     bytes = bytearray.fromhex(hex_string)
-    # This returns a byte object, and thus is prefixed with a 'b'; the comparison will thus fail because it is not a string; hence, the decode call added below. 
-    converted_base64 = base64.b64encode(bytes).decode('utf-8')
+    # This returns a byte object, and thus is prefixed with a 'b'; the comparison will thus fail because it is not a string; hence, the decode call added below.
 
+    converted_base64 = base64.b64encode(binascii.unhexlify(hex_string)).decode('ascii')
+    
     if converted_base64 == 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t':
         print(f"Converted to: {converted_base64}")
 
