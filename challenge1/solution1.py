ebc56a9a5a21da157aa4b3d34eb73d404a1a98c5
Alexis La Porte <aklaporte@gmail.com>
2016-10-12 22:00:44 -0400

    refactor and rename variables for clarity

--- challenge1/solution1.py
+++ challenge1/solution1.py
@@ -1,14 +1,12 @@
 import base64
 import binascii
 
-def toBase64(hex_string):
-    bytes = bytearray.fromhex(hex_string)
-    # This returns a byte object, and thus is prefixed with a 'b'; the comparison will thus fail because it is not a string; hence, the decode call added below.
-
-    converted_base64 = base64.b64encode(binascii.unhexlify(hex_string)).decode('ascii')
-    
-    if converted_base64 == 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t':
-        print(f"Converted to: {converted_base64}")
+def toBase64(hex_value):
+    # decode input from hex to binary
+    binary_output = binascii.unhexlify(hex_value)
+    # convert binary to base64
+    base64_output = base64.b64encode(binascii.unhexlify(hex_value))
+    print(str(base64_output)[1:])
 
 toBase64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 )
