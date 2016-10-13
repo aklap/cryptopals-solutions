1ac0247c8892e6589070248afc21fa176f2aad70
Alexis La Porte <aklaporte@gmail.com>
2016-10-13 19:19:36 -0400

    import binascii

--- challenge5/solution5.py
+++ challenge5/solution5.py
@@ -1,4 +1,5 @@
 from key import *
+import binascii
 
 def decode(plaintext, string):
     expected = '0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
@@ -8,8 +9,7 @@ def decode(plaintext, string):
 
     for byte in plaintext:
         xbyte = byte ^ ord(key.chars[0])
-
-        if len(hex(xbyte)) < 4:
+        if len(xbyte) < 4:
             formatted_xbyte = hex(xbyte).replace('0x', '0')
         else:
             formatted_xbyte = hex(xbyte)[2:]
