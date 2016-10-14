a77642efc681c1b2a01dcbbabcb1b04c949661f8
Alexis La Porte <aklaporte@gmail.com>
2016-10-14 14:10:58 -0400

    change incorrect name of function

--- challenge5/solution5.py
+++ challenge5/solution5.py
@@ -1,7 +1,7 @@
 from key import *
 import binascii
 
-def decode(plaintext, string):
+def encrypt(plaintext, string):
     expected = '0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
     key = Key(string)
     plaintext = bytearray(plaintext, 'utf-8')
@@ -16,10 +16,10 @@ def decode(plaintext, string):
         
         ciphertext.append(formatted_xbyte)
         key.rotate()
-        
+
     ciphertext_to_string = ''.join(ciphertext)    
 
     if ciphertext_to_string == expected:
         print(ciphertext_to_string)
 
-decode("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal", 'ICE')
+encrypt("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal", 'ICE')
