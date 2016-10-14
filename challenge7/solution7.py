7b32470c5932fc5af2d68181ff0873c8127e8c57
Alexis La Porte <aklaporte@gmail.com>
2016-10-13 23:02:34 -0400

    add solution

--- challenge7/solution7.py
+++ challenge7/solution7.py
@@ -1,6 +1,16 @@
-require ‘openssl’
+import base64
+from Crypto.Cipher import AES
 
-    def decrypt():
-        key = "YELLOW SUBMARINE"
-        # Easiest way: use OpenSSL::Cipher and give it AES-128-ECB as the cipher.
-    
+# Well this was perfectly awful. I went to Python for crypto libs, and most all of crypto libraries are not for 3+. Had trouble installing via 2 different libs via pip for 3, and one required knowledge of a nonce/initialization vector, so ended up using 2.7 & pycrypto for this. 
+
+with open('ciphertext.txt', 'rb') as f:
+    while True:
+        ciphertext = base64.b64decode(f.read()).rstrip()
+        key = b'YELLOW SUBMARINE'
+        cipher = AES.new(key, AES.MODE_ECB)
+        plaintext = cipher.decrypt(ciphertext)
+
+        print(plaintext)
+
+        break
+    f.close()
