f1e091b42cada77956dec2e374be1d674c2cee1e
Alexis La Porte <aklaporte@gmail.com>
2016-10-15 16:02:57 -0400

    return instead of print string

--- challenge1/solution1.py
+++ challenge1/solution1.py
@@ -2,11 +2,12 @@ import base64
 import binascii
 
 def toBase64(hex_value):
-    # decode input from hex to binary
+    # decode input from hex to bytes
     binary_output = binascii.unhexlify(hex_value)
-    # convert binary to base64
+    
+    # convert bytes to base64
     base64_output = base64.b64encode(binascii.unhexlify(hex_value))
-    print(str(base64_output)[1:])
+    return str(base64_output)[1:]
 
 toBase64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 )
