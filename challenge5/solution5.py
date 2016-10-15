0b7f77acfc65c83d6f6343fe769f202e892809cd
Alexis La Porte <aklaporte@gmail.com>
2016-10-15 16:53:14 -0400

    remove print statment, expected string variable to test file

--- challenge5/solution5.py
+++ challenge5/solution5.py
@@ -2,7 +2,6 @@ from key import *
 import binascii
 
 def encrypt(plaintext, string):
-    expected = '0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
     key = Key(string)
     plaintext = bytearray(plaintext, 'utf-8')
     ciphertext = []
@@ -18,8 +17,5 @@ def encrypt(plaintext, string):
         key.rotate()
 
     ciphertext_to_string = ''.join(ciphertext)    
+    return ciphertext_to_string
 
-    if ciphertext_to_string == expected:
-        print(ciphertext_to_string)
-
-encrypt("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal", 'ICE')
