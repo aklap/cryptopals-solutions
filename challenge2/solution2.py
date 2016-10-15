9504201239d3bde16392a6468dc20f1a8ef8cc9c
Alexis La Porte <aklaporte@gmail.com>
2016-10-15 16:29:52 -0400

    fix failing test, convert hex number to decimal

--- challenge2/solution2.py
+++ challenge2/solution2.py
@@ -3,14 +3,14 @@ import sys
 def getXOR(file1, file2):
 
     with open(str(file1), "rb") as a, open(str(file2), "rb") as b:
+
         xor_result = [];
 
         while True:
             a_byte = a.read(1)
             b_byte = b.read(1)
-
             if a_byte.decode('ascii') != '':
-                xbyte = ord(a_byte)^ord(b_byte)
+                xbyte = int(a_byte, 16)^int(b_byte, 16)
                 res_byte = format(xbyte, 'x')
                 xor_result.append(res_byte)
 
