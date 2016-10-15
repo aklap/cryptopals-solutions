a19c34a0896f8f753c5e58b016b6a4ce355725a3
Alexis La Porte <aklaporte@gmail.com>
2016-10-15 14:09:22 -0400

    update solution to work for Python v 3+

--- challenge7/solution7.py
+++ challenge7/solution7.py
@@ -1,8 +1,6 @@
 import base64
 from Crypto.Cipher import AES
 
-# Well this was perfectly awful. I went to Python for crypto libs, and most all of crypto libraries are not for 3+. Had trouble installing via 2 different libs via pip for 3, and one required knowledge of a nonce/initialization vector, so ended up using 2.7 & pycrypto for this. 
-
 with open('ciphertext.txt', 'rb') as f:
     while True:
         ciphertext = base64.b64decode(f.read()).rstrip()
@@ -10,7 +8,7 @@ with open('ciphertext.txt', 'rb') as f:
         cipher = AES.new(key, AES.MODE_ECB)
         plaintext = cipher.decrypt(ciphertext)
 
-        print(plaintext)
+        print(plaintext.decode('ascii'))
 
         break
     f.close()
