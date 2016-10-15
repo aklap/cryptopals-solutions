2ed8264b9685fd2b28aabf75e09143aca7889665
Alexis La Porte <aklaporte@gmail.com>
2016-10-15 16:13:49 -0400

    change how string is encoded for pretty printing

--- challenge1/solution1.py
+++ challenge1/solution1.py
@@ -7,7 +7,7 @@ def toBase64(hex_value):
     
     # convert bytes to base64
     base64_output = base64.b64encode(binascii.unhexlify(hex_value))
-    return str(base64_output)[1:]
+    return base64_output.decode('utf-8')
 
 toBase64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 )
