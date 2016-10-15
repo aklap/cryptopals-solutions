44074da700480a50e85e78cbe1fdc3cf3dd59c32
Alexis La Porte <aklaporte@gmail.com>
2016-10-15 17:29:34 -0400

    remove print statement, return plaintext variable;

--- challenge7/solution7.py
+++ challenge7/solution7.py
@@ -1,14 +1,17 @@
 import base64
 from Crypto.Cipher import AES
 
-with open('ciphertext.txt', 'rb') as f:
-    while True:
-        ciphertext = base64.b64decode(f.read()).rstrip()
-        key = b'YELLOW SUBMARINE'
-        cipher = AES.new(key, AES.MODE_ECB)
-        plaintext = cipher.decrypt(ciphertext)
+def decrypt_AES():
+    plaintext = ""
+    with open('ciphertext.txt', 'rb') as f:
+        while True:
+            ciphertext = base64.b64decode(f.read()).rstrip()
+            key = b'YELLOW SUBMARINE'
+            cipher = AES.new(key, AES.MODE_ECB)
+            plaintext_bytes = cipher.decrypt(ciphertext)
+            plaintext = plaintext_bytes.decode()
+            break
+        f.close()
 
-        print(plaintext.decode('ascii'))
+    return plaintext
 
-        break
-    f.close()
