4ab29dfb5d8c60e71ed83878e6c99d267a63fbce
Alexis La Porte <aklaporte@gmail.com>
2016-10-17 22:49:07 -0400

    refactor function

--- set_1/challenge4/solution4.py
+++ set_1/challenge4/solution4.py
@@ -4,26 +4,14 @@ from solution3 import freq_decode
 
 # save all the high scores and then get the max score to find winning string
 
-def decrypt(file):
+def find_xor(file):
     with open(str(file), "rb") as f:
 
         while True:
-            results = []
-
-            # readline() caused unhexlify() in solution3 file to throw odd length error
-            for line in f:
-                result = freq_decode(line.strip())
-                results.append(result)
-
+            results = [freq_decode(line.strip()) for line in f]
             winner = max(results, key=lambda result: result[0])
 
-            print(f'The highest score was: {winner[0]}')
-            print(f'The key was: {winner[1]}')
-            print(f'The plaintext is: {winner[2]}')
-
             return (winner)
-
-            if not f.read(1):
-                break
+            break
     f.close()
 
