0737f7913eb95e1631874b2a5762212c9df6c6da
Alexis La Porte <aklaporte@gmail.com>
2016-10-17 23:20:03 -0400

    refactor function

--- set_1/challenge4/solution4.py
+++ set_1/challenge4/solution4.py
@@ -1,6 +1,6 @@
 import sys
-import frequencies
-from solution3 import freq_decode
+sys.path.append('../challenge3')
+from solution3 import decrypt_xor
 
 # save all the high scores and then get the max score to find winning string
 
@@ -8,10 +8,9 @@ def find_xor(file):
     with open(str(file), "rb") as f:
 
         while True:
-            results = [freq_decode(line.strip()) for line in f]
-            winner = max(results, key=lambda result: result[0])
-
-            return (winner)
+            results = [decrypt_xor(line.strip()) for line in f]
+            best_match = max(results, key=lambda result: result[0])
+            return (best_match)
             break
     f.close()
 
