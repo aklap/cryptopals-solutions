fecf277e54186500c02b4425cc5fd2dde07886a6
Alexis La Porte <aklaporte@gmail.com>
2016-10-17 23:13:17 -0400

    change test to check score

--- set_1/challenge3/test_solution3.py
+++ set_1/challenge3/test_solution3.py
@@ -5,7 +5,7 @@ example = decrypt_xor('1b37373331363f78151b7f2b783431333d78397828372d363c78373e7
 
 class MyTest(unittest.TestCase):
     def test(self):
-        self.assertEqual(example, ('X', 'Cooking MC\'s like a pound of bacon'))
+        self.assertEqual(example, (345.46000000000015, 'X', 'Cooking MC\'s like a pound of bacon'))
 
 if __name__ == '__main__':
     unittest.main()
