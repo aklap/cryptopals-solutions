fabe2617bd571261a3d172f1336007f9f8a1bf3e
Alexis La Porte <aklaporte@gmail.com>
2016-10-17 20:20:36 -0400

    remove encoding

--- set_1/challenge1/solution1.py
+++ set_1/challenge1/solution1.py
@@ -7,4 +7,4 @@ def toBase64(hex_value):
     
     # convert bytes to base64
     base64_output = base64.b64encode(binascii.unhexlify(hex_value))
-    return base64_output.decode('utf-8')
+    return base64_output.decode()
