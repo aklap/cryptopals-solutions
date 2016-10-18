ae1769fc7f81cb43b9cb41576ebe6dda8573ecbb
Alexis La Porte <aklaporte@gmail.com>
2016-10-17 23:56:15 -0400

    change function name in test

--- set_1/challenge5/test_solution5.py
+++ set_1/challenge5/test_solution5.py
@@ -1,7 +1,7 @@
 import unittest
-from solution5 import encrypt
+from solution5 import xor_encrypt
 
-example = encrypt("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal", 'ICE')
+example = xor_encrypt("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal", 'ICE')
 
 expected = '0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
 
