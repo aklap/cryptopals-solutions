2e9cd93f9a0da40f2694c179ac649062fa7db68b
Alexis La Porte <aklaporte@gmail.com>
2019-01-31 22:22:38 -0500

    Challenge 4: Refactor solution.

--- set_1/challenge4/test_solution4.py
+++ set_1/challenge4/test_solution4.py
@@ -5,7 +5,7 @@ example = find_xor("4.txt")
 
 class MyTest(unittest.TestCase):
     def test(self):
-        self.assertEqual(example, (443.51, '5', 'Now that the party is jumping\n'))
+        self.assertEqual(example, 'Now that the party is jumping\n')
 
 if __name__ == '__main__':
     unittest.main()
